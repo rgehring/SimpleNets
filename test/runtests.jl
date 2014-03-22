d56e0c740597a20ff473b6b95af3769f31ad9ab4
Ryan Gehring <ryangehrin@gmail.com>
2014-03-22 04:12:51 -0700

    added monotonicity validator and unit tests, started to skeleton neural net class

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,4 +7,8 @@ using Base.Test
 println("Test SingleLayerPerceptron has been included")
 @test SimpleNets.SingleLayerPerceptron.HELP == "Single layer perceptron, offers train, score, and validate public methods"
 
+#Validations unit tests
+@test SimpleNets.Validations.isMonotonic( x -> 2*x ) 
+@test !SimpleNets.Validations.isMonotonic( x -> -2*x ) 
+
 
