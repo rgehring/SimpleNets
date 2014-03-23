401360c0ac8945d6299ab669210d2e586d3baef7
Ryan Gehring <ryangehrin@gmail.com>
2014-03-22 17:04:09 -0700

    added scripts dir for more detailed statistical validation of algorithms and refactored tests into multiple files

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,8 @@
 using SimpleNets
 using Base.Test  
 
-# write your own tests here
-@test 1 == 1
+# Validations Utilities
+include( "util/validations.jl" )
 
-println("Test SingleLayerPerceptron has been included")
-@test SimpleNets.SingleLayerPerceptron.HELP == "Single layer perceptron, offers train, score, and validate public methods"
-
-#Validations unit tests
-@test SimpleNets.Validations.isMonotonic( x -> 2*x ) 
-@test !SimpleNets.Validations.isMonotonic( x -> -2*x ) 
 
 
